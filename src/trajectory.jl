5af9e0ebc440d13ce20c190a9fe074c6a7b016dc
Nelson Morrow <nelsonmorrow@utexas.edu>
2021-11-01 12:00:40 -0500

    Refactor to construct BSplineCurve

--- src/trajectory.jl
+++ src/trajectory.jl
@@ -15,8 +15,8 @@
 # %%
 
 include("bspline.jl")
-using LinearAlgebra, Plots
-export reconstruct_trajectory_1d, reconstruct_trajectory_3d, generate_helix_test_tangents
+using LinearAlgebra
+export reconstruct_trajectory, generate_helix_test_tangents
 
 function generate_helix_test_tangents(r::Real=1, c::Real=1, number_of_points::Integer=100)
     t = LinRange(0, 100, number_of_points)
@@ -46,22 +46,21 @@ function create_knot_vector(number_of_data_points::Integer,
     kv
 end
 
-function reconstruct_trajectory_3d(tangents::Matrix{<:Float64}, number_of_control_points::Integer=(length(tangents) ÷ 2))
-  arr = tangents
-  for i in 1:1:2
-    control_points, arr[:,i] = reconstruct_trajectory_1d(tangents[:,i])
-  end
-  fig = plot((arr[:,1], arr[:,2], arr[:,3]))
-  savefig(fig, joinpath(@__DIR__, "../examples/reconstructed_helix.png"))
-end
-function reconstruct_trajectory_1d(tangents::Vector{<:Float64}, number_of_control_points::Integer=(length(tangents) ÷ 2))
-  kv = create_knot_vector(length(tangents), number_of_control_points)
-  knot_space_samples = 0:1/(length(tangents)-1):1
+function reconstruct_trajectory(tangents_matrix::Matrix{<:Float64})
+  kv = create_knot_vector(size(tangents_matrix,1))
+  knot_space_samples = 0:1/(size(tangents_matrix,1)-1):1
+  kv = prepend!(collect(knot_space_samples), Vector{Float64}([0.; 0.; 0.; 0.]))
+  kv = append!(kv, [1.; 1.; 1.; 1.])
   basis = BSplineBasis(kv, 3, k=1)
-  N, Nprime = construct_spline_matrix(basis, collect(knot_space_samples), length(kv))
-  A = pinv(Nprime' * Nprime)
-  B = (Nprime' * tangents)
-  control_points, curve = A*B, Nprime*(A*B)
+  control_points = zeros(Float64, (length(kv), size(tangents_matrix, 2)))
+  for col in 1:1:size(tangents_matrix,2)
+    tangents = tangents_matrix[:,col]
+    N, Nprime = construct_spline_matrix(basis, collect(knot_space_samples), length(kv))
+    A = pinv(Nprime' * Nprime)
+    B = (Nprime' * tangents)
+    control_points[:, col] = A*B
+  end
+  curve = BSplineCurve(basis, control_points)
 end
 
 function construct_spline_matrix(basis::BSplineBasis, samples::Vector{<:Float64}, num_knots::Integer)
@@ -75,8 +74,8 @@ function construct_spline_matrix(basis::BSplineBasis, samples::Vector{<:Float64}
   N, Nprime
 end
 
-
 function predict(x)
 end
+
 function loss(x)
 end
