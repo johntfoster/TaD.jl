16085c60e298ca838bdb142fca7554fe020a49af
Nelson Morrow <nelsonmorrow@utexas.edu>
2022-10-11 18:15:44 -0500

    Broken

--- test/test_trajectory.jl
+++ test/test_trajectory.jl
@@ -13,9 +13,20 @@
 # limitations under the License.
 using Test
 using TaD
-import TaD: construct_helix_tangents, evaluate
+using LinearAlgebra
+import TaD: evaluate, asc, reconstruct_trajectory
 
 @testset "reconstruct trajectory of helix in 3D" begin
+    function helix(;Ω1::Tuple=(0.,4*π), Ω2::Tuple=(0.,4*π), n::Int64=100, α::Tuple=(1.,1.,1.))
+        """
+        Constructs nx3 Matrix [α1cos.(θ) α2sin.(θ), α3LinRange(Ω, n+1)]
+        """
+        arr = zeros(Float64, (n+1, 3))
+        arr[:, 1] = α[1]*cos.(LinRange(Ω1[1], Ω1[2], n+1))
+        arr[:, 2] = α[2]*sin.(LinRange(Ω2[1], Ω2[2], n+1))
+        arr[:, 3] = α[3]*LinRange(0, Ω1[2]-Ω1[1], n+1)
+        arr
+    end
     λ = helix(n=1000) #Helix Derivative
     Λ = helix(Ω1=(-π/2, 4*π-π/2), Ω2=(π/2, 4*π+π/2), n=1000, α=(1,-1,1)) # Original Helix
     tup = asc(λ[:,3], λ, [0.0,-1.,0.])
@@ -24,8 +35,14 @@ import TaD: construct_helix_tangents, evaluate
 end
 
 @testset "Abughaban test data" begin
+    function asc_tangents(MD, θ, ϕ)
+        λ_E   = sin.(ϕ) .* sin.(θ)
+        λ_N   = sin.(ϕ) .* cos.(θ)
+        λ_TVD = cos.(ϕ)
+        return [λ_E λ_N λ_TVD] #x y z
+    end
     MD = collect(5000:100:5900)
-    θ, ϕ = to_radian(collect(0:5:45)), to_radian(collect(0:10:90))
+    θ, ϕ = (2π/360) .* collect(0:5:45), (2π/360) .* collect(0:10:90)
     λ = asc_tangents(MD, θ, ϕ)
     tup = asc(MD, λ, [0., 0., 5000.])
     Curve = reconstruct_trajectory(hcat(tup...))
