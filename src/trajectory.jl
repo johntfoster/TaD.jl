0835f616373412384fc9b98e6f3422ed56f1f80c
Nelson Morrow <nelsonmorrow@utexas.edu>
2022-10-11 17:29:47 -0500

    Better helix function; cleanup trajectory

--- src/trajectory.jl
+++ src/trajectory.jl
@@ -16,32 +16,8 @@
 
 # %%
 include("bspline.jl")
-using LinearAlgebra, IterativeSolvers, Plots
-export construct_spline_matrix, reconstruct_trajectory, construct_helix, L2error, main, construct_helix_tangents, construct_non_helix
-
-function construct_helix_tangents(n::Integer = 100)
-    """
-    Helper function which outputs an nx3 array of form [cos.(x)' sin.(x)'  linspace(0,1,n+1)] 
-    """
-    t = LinRange(0, 4*π, n+1)
-    arr = zeros(Float64, (length(t), 3))
-    arr[:, 1] = cos.(t)
-    arr[:, 2] = sin.(t)
-    arr[:, 3] = LinRange(0, 4*π, n+1)
-    arr
-end
-
-function construct_helix(n::Integer = 100)
-    """
-    Helper function which outputs an nx3 array of form [cos.(x)' sin.(x)'  linspace(0,1,n+1)] 
-    """
-    t = LinRange(0, 4*π, n+1)
-    arr = zeros(Float64, (length(t), 3))
-    arr[:, 1] = sin.(t)
-    arr[:, 2] = -cos.(t)
-    arr[:, 3] = LinRange(0, 4*π, n+1)
-    arr
-end
+using LinearAlgebra, IterativeSolvers
+export reconstruct_trajectory, create_knot_vector
 
 function create_knot_vector(Qk::Array{<:Real}, p::Integer = 3)
     ū = create_ūk(Qk)
@@ -53,17 +29,6 @@ function create_knot_vector(Qk::Array{<:Real}, p::Integer = 3)
     end
     kv[(end - p):end] .= 1
     kv
-end
-
-function reconstruct_control_points(Q::Array{<:Real}, u::Vector{<:Real}, p::Integer=3)
-    P = zeros(size(Q))
-    P[1,:] .= 0
-    for j=1:size(Q,2)
-        for i=2:size(Q,1) #Iterate through the columns
-            P[i,j] = Q[i,j]*(u[i+p+1] - u[i+1])/p - P[i-1,j]
-        end
-    end
-    P
 end
 
 #From definition, m (Num Knots) = n (Num points) + p (Polynomial Degree) + 1
@@ -89,24 +54,4 @@ function construct_spline_matrix(basis::BSplineBasis, samples::Vector{<:Real}, n
         N[i,column-p:column] = evals[1,:]#evals[2, :][evals[2, :] .> 0]
     end
     N
-end
-
-function L2error(C::BSplineCurve, tangents::Array{<:Real})
-    """
-    Returns a vector with the cumulative sum of the L2 error for each dimension.
-    """
-    evals = evaluate(C, length(tangents[:,1]))
-    error = zeros(length(tangents[:,1]))
-    for (i, u) in enumerate(evals[:,3]')
-        index = findall(x-> x>=u, tangents[:,3])[1] #Find index of tangents vec closest to z axis of our control point
-        error[i] = abs(tangents[index,2] - evals[i,2]) + abs(tangents[index,3] - evals[i,3])
-    end
-    return error
-end
-
-function main(n::Integer=100, p::Integer=3)
-    Q = construct_helix(n)#[0:.05:4 sin.(0:.05:4)]#construct_non_helix(n) # Textbook Example: 
-    Curve = reconstruct_trajectory(Q, p)
-    plot(Curve, label="Bspline")
-    plot!(tuple(eachcol(Q)...), label="Original Helix")
 end
